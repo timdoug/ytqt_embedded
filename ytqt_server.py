bda29ea636e7742b6bf6d2a12a2c5122ffc2a1ed
Tim Douglas <me@timdoug.com>
2013-08-22 00:07:05 -0700

    Do a better job of running only on real YouTube video pages

--- ytqt_server.py
+++ ytqt_server.py
@@ -1,3 +1,4 @@
+import urlparse
 import subprocess
 
 from flask import Flask, make_response, request
@@ -7,6 +8,8 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     yt_url = request.args.get('url', '')
+    if len(urlparse.parse_qs(urlparse.urlparse(yt_url).query).get('v', [''])[0]) != 11:
+        return 'Bad URL'
     vid_url = subprocess.Popen('youtube-dl -g -f 38/37/22/18 "%s"' % yt_url, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True).communicate()
     return make_response(vid_url[0].strip(), 200, {'Access-Control-Allow-Origin': '*'})
 
